5f055441d485270996ff7aeb9189a2811d44fb68
Scarstack Solution <137973466+scarstacksolution@users.noreply.github.com>
2024-07-16 02:19:04 -0500

    Update start_server.sh

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -11,14 +11,14 @@ sudo systemctl daemon-reload
 
 # Enable the service to start on boot
 echo "Enabling my-dotnet-app.service..."
-sudo systemctl enable my-dotnet-app.service
+sudo systemctl enable BlazorApp.service
 
 # Start the service
 echo "Starting my-dotnet-app.service..."
-sudo systemctl start my-dotnet-app.service
+sudo systemctl start BlazorApp.service
 
 # Check the status of the service
 echo "Checking status of my-dotnet-app.service..."
-sudo systemctl status my-dotnet-app.service
+sudo systemctl status BlazorApp.service
 
 echo "start_server.sh script completed successfully."
